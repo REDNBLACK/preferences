5ede35d04db59ac81f868c7eb5d973fe724b0c61
REDNBLACK <boris.p@protonmail.com>
2023-04-04 13:48:37 +0300

    Fix PGP Secure SSH Script

--- pgp/secure-ssh.zsh
+++ pgp/secure-ssh.zsh
@@ -2,14 +2,14 @@
 emulate -LR zsh
 
 () {
-  local ssh_cfg=/etc/ssh/ssh_config
-  local sshd_cfg=/etc/ssh/sshd_config
+  local ssh_cfg='/etc/ssh/ssh_config'
+  local sshd_cfg='/etc/ssh/sshd_config'
   local store="$XDG_CONFIG_HOME/ssh"
+  local keys=('id_rsa' 'id_dsa' 'id_ecdsa' 'id_ed25519')
 
   sudo sed -i '' -n -e '/^[# ]*UserKnownHostsFile .*$/!p' -e '$a\'$'\n'"    UserKnownHostsFile $store/known_hosts" $ssh_cfg
-  for id in (id_rsa id_dsa id_ecdsa id_ed25519); do
+  for id in $keys; do
     sudo sed -i '' -n -e '/^[# ]*IdentityFile .*'$id'$/!p' -e '$a\'$'\n'"    IdentityFile $store/$id" $ssh_cfg
-    echo $id
   done
 
   sudo sed -i '' -n -e '/^[# ]*AuthorizedKeysFile .*$/!p' -e '$a\'$'\n'"AuthorizedKeysFile $store/authorized_keys" $sshd_cfg
